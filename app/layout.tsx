294baff705c2bb2d629f8784075403763cecf2d5
Hrvoje Cuckovic <cuckovichrvoje@gmail.com>
2025-10-15 12:59:57 +0200

    fix: changed file names, removed additional files, moved styling to separated file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import "./layout.css";
 // Nextra theme must be imported after index.css so PandaCSS doesn't override default nextra styles
 import "nextra-theme-docs/style.css";
 
@@ -9,14 +10,11 @@ import { Layout } from "nextra-theme-docs";
 
 import { Footer } from "~components/sections/footer/footer";
 import { Navbar } from "~components/sections/navbar/navbar";
-import SectionHandler from "~components/ui/section-handler";
-import { css } from "~styled-system/css/css";
+import MobileSidebarToggle from "~components/ui/mobile-sidebar-toggle";
+// import { css } from "~styled-system/css/css";
 import { Box } from "~styled-system/jsx";
 
 import { fontClassName } from "../styles/fonts";
-
-const navbar = <Navbar />;
-const footer = <Footer />;
 
 export default async function RootLayout({ children }: { readonly children: React.ReactNode }) {
     return (
@@ -35,108 +33,13 @@ export default async function RootLayout({ children }: { readonly children: Reac
                 {/* Your additional tags should be passed as `children` of `<Head>` element */}
             </Head>
             <body>
-                <Box
-                    className={css({
-                        backgroundColor: "neutral.midnight",
-                        position: "relative",
-                        "& .nextra-sidebar": {
-                            position: "relative",
-                            top: 0,
-                            userSelect: "auto",
-                            opacity: 1,
-                            visibility: "visible",
-                            borderRight: "0.5px solid",
-                            borderColor: "neutral.800",
-                            display: "block",
-                            width: { base: "100vw", md: "252px" },
-                            minWidth: "252px",
-                            height: "auto",
-                            transition: "all 0.3s ease-in-out",
-                        },
-                        "& .nextra-scrollbar li div": {
-                            width: { base: "100%", md: "200px" },
-                        },
-                        "& .nextra-sidebar.hidden": {
-                            opacity: 0,
-                            userSelect: "none",
-                            visibility: "hidden",
-                            position: "absolute",
-                            top: "-10%",
-                            transition: "all 0.3s ease-in-out",
-                        },
-                        "& div:has(aside)": {
-                            display: "flex",
-                            flexDirection: { base: "column", md: "row" },
-                        },
-                        "& .active a": {
-                            color: "electric.200",
-                            fontWeight: "600",
-                            borderRadius: "4px",
-                            backgroundColor: "darkElement.100",
-                            position: "relative",
-                            "&::after": {
-                                content: "''",
-                                position: "absolute",
-                                left: 0,
-                                top: "50%",
-                                transform: "translateY(-50%)",
-                                width: "2px",
-                                height: "24px",
-                                backgroundColor: "electric.200",
-                                overflow: "hidden",
-                                borderRadius: "4px",
-                            },
-                        },
-                        "& article": {
-                            maxWidth: "1054px",
-                        },
-                        "& .nextra-scrollbar .open ul": {
-                            paddingLeft: "0px",
-                            paddingTop: "15px",
-                        },
-                        "& .nextra-toc, & .nextra-sidebar": {
-                            paddingTop: { base: "0px", md: "10px" },
-                        },
-                        "& .nextra-toc div": {
-                            "&::before": {
-                                position: "absolute",
-                                content: "''",
-                                width: "1px",
-                                height: "80%",
-                                top: "7px",
-                                left: "-10px",
-                                backgroundColor: "darkElement.200",
-                            },
-                        },
-                        "& .nextra-scrollbar a": {
-                            fontWeight: "normal",
-                        },
-                        "& article .nextra-breadcrumb a:nth-child(1)": {
-                            "&::before": {
-                                content: "''",
-                            },
-                            "&::after": {
-                                content: "''",
-                            },
-                            padding: "0px",
-                        },
-                        "& article div a:first-child, & article div a:last-child": {
-                            "&::before": {
-                                fontSize: "12px",
-                                width: "max-content",
-                                height: "16px",
-                                color: "neutral.400",
-                            },
-                            padding: "10px 30px",
-                        },
-                    })}
-                >
+                <Box className="layout">
                     <Layout
                         banner={false}
-                        navbar={navbar}
+                        navbar={<Navbar />}
                         pageMap={await getPageMap()}
-                        docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
-                        footer={footer}
+                        docsRepositoryBase="https://github.com/Treblle/treblle-documentation"
+                        footer={<Footer />}
                         darkMode={false}
                         sidebar={{ toggleButton: false }}
                         editLink={false}
@@ -144,7 +47,7 @@ export default async function RootLayout({ children }: { readonly children: Reac
                         toc={{ backToTop: false }}
                         navigation={false}
                     >
-                        <SectionHandler title="Treblle Docs" sectionClass="nextra-sidebar" />
+                        <MobileSidebarToggle title="Treblle Docs" sectionClass="nextra-sidebar" />
                         {children}
                     </Layout>
                 </Box>
---
+++ components/ui/mobile-sidebar-toggle.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Button } from "nextra/components";
+import { useState } from "react";
+
+import { css } from "~styled-system/css/css";
+import { Box } from "~styled-system/jsx";
+
+import { Icon } from "../../theme/icons";
+
+type Properties = {
+    readonly title?: string;
+    readonly sectionClass?: string;
+};
+
+export default function MobileSidebarToggle({ title, sectionClass }: Properties) {
+    const [isOpen, setIsOpen] = useState(true);
+    function toggleSection() {
+        setIsOpen(!isOpen);
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        document.querySelector(`.${sectionClass}`)?.classList.toggle("hidden");
+    }
+
+    const icon = <Icon icon="ArrowDown" className={css({ transition: "transform 0.2s", transform: isOpen ? "rotate(180deg)" : "rotate(0deg)" })} />;
+
+    return (
+        <Box
+            className={css({
+                padding: "32px 16px 0px 16px",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "8px",
+                display: { base: "flex", md: "none" },
+            })}
+        >
+            <h1 className={css({ color: "neutral.400" })}>{title}</h1>
+
+            <Button className={css({ color: "neutral.400" })} onClick={toggleSection}>
+                {" "}
+                {icon}
+            </Button>
+        </Box>
+    );
+}
