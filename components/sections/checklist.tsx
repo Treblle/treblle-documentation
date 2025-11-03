87d1eaa45b1d42eed395acdff2ba84be37453615
Hrvoje Cuckovic <cuckovichrvoje@gmail.com>
2025-11-03 09:46:33 +0100

    feat: added checkbox component

---
+++ components/sections/checklist.tsx
@@ -0,0 +1,60 @@
+import { css } from "~styled-system/css";
+import { Box } from "~styled-system/jsx";
+
+type ChecklistProperties = {
+    readonly children?: React.ReactNode;
+};
+
+export function Checklist({ children }: ChecklistProperties) {
+    return (
+        <Box
+            className={css({
+                marginTop: "16px",
+            })}
+        >
+            {children}
+        </Box>
+    );
+}
+
+Checklist.item = function ChecklistItem({ children }: ChecklistProperties) {
+    return (
+        <Box
+            className={css({
+                display: "flex",
+                alignItems: "center",
+                gap: "9px",
+                marginBottom: "16px",
+            })}
+        >
+            <input
+                type="checkbox"
+                className={css({
+                    appearance: "none",
+                    width: "14px",
+                    height: "14px",
+                    borderRadius: "3px",
+                    border: "1px solid #bcc0d9",
+                    cursor: "pointer",
+                    position: "relative",
+                    "&:checked": {
+                        backgroundColor: "#0058ff",
+                        borderColor: "#0058ff",
+                        "&::after": {
+                            content: "''",
+                            position: "absolute",
+                            left: "50%",
+                            top: "50%",
+                            transform: "translate(-50%, -60%) rotate(45deg)",
+                            width: "3px",
+                            height: "7px",
+                            border: "solid white",
+                            borderWidth: "0 1px 1px 0",
+                        },
+                    },
+                })}
+            />
+            {children}
+        </Box>
+    );
+};
