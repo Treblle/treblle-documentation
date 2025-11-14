3972c1ae131a926e1c5c317af62c25d06dcd1820
Hrvoje Cuckovic <cuckovichrvoje@gmail.com>
2025-11-14 15:49:39 +0100

    fix: returned meta.tsx

---
+++ app/_meta.tsx
@@ -0,0 +1,27 @@
+import { Search } from "nextra/components";
+
+import { css } from "~styled-system/css/css";
+
+export default {
+    "--": {
+        type: "separator",
+        title: (
+            <div
+                className={css({
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "2",
+                    width: { base: "80vw", md: "200px" },
+                })}
+            >
+                <Search className={css({ width: "1000px" })} placeholder="Search..." />
+            </div>
+        ),
+    },
+    index: {
+        title: "Quick start",
+        theme: {
+            breadcrumb: false,
+        },
+    },
+};
