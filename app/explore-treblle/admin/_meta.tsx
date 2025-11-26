e8d49f83066d18836c8eedf66cffc40133b161fe
Hrvoje <cuckovichrvoje@gmail.com>
2025-11-26 14:09:35 +0100

    Fix/mar 1833/fix responzive design
    * fix: fixed landing page
    
    * fix: fixed pagination paths
    
    * fix: fixed pagination cards problem
    
    * fix: removed permissions from meta file

--- app/explore-treblle/admin/_meta.tsx
+++ app/explore-treblle/admin/_meta.tsx
@@ -1,6 +1,5 @@
 export default {
   "roles-and-permissions": "Roles and Permissions",
-  "permissions-reference": "Permissions Reference",
   "user-management": "User Management",
   "sso": "Single Sign-On",
   "billing": "Billing and Subscription Management",
