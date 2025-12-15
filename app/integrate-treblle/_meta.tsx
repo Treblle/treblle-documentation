bd9fb45fe7e5ce5251da9a2a32d8b4f398f51bd8
Rahulkhinchi03 <rahulkhinchirk7@gmail.com>
2025-12-15 17:53:19 +0530

    Updated dotnet-core docs and changed sdk to integrations

--- app/integrate-treblle/_meta.tsx
+++ app/integrate-treblle/_meta.tsx
@@ -1,7 +1,7 @@
 export default {
     "integrations": "Treblle Integrations",
-    "custom-sdk": "Custom SDK",
+    "custom-integration": "Custom Integration",
     "github-integration": "GitHub App",
     "api-gateways": "API Gateways",
-    "sdks": "SDKs",
+    "Integration": "Integrations",
   };
