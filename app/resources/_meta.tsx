5b4b41052c3f95151b63c4d636f072bafe579ad9
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2025-12-23 15:13:49 +0530

    Created first set of use case tutorials (#51)

--- app/resources/_meta.tsx
+++ app/resources/_meta.tsx
@@ -1,6 +1,7 @@
 export default {
-    "faqs": "FAQs",
-    "api-knowledgebase": "API Knowledgebase",
-    "laravel-course": "Laravel API Course",
-    "json-schema": "JSON Schema",
-  }
+  "use-case-tutorials": "Use Case Tutorials",
+  "faqs": "FAQs",
+  "api-knowledgebase": "API Knowledgebase",
+  "laravel-course": "Laravel API Course",
+  "json-schema": "JSON Schema",
+}
---
+++ app/resources/use-case-tutorials/_meta.tsx
@@ -0,0 +1,7 @@
+export default {
+    "monitor-first-api": "Monitor Your First API in 5 Minutes",
+    "setup-custom-alerts": "Set Up Custom Alerts",
+    "debug-production-issue": "Debug a Production Issue",
+    "customer-specific-issue": "Track Down a Customer-Specific Issue",
+    "setup-multi-environment": "Set Up Multi-Environment Monitoring",
+}
