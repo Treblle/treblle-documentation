a9e3ace9b2f4dc14439061a89d034e0168b1eb87
miki <imikodanic@gmail.com>
2026-03-13 14:06:55 +0100

    feat: roles permissions
    * bruno test homepage
    
    * bruno test
    
    * bruno test 2
    
    * ispravljeno final
    
    * is it finall over?
    
    * now finally over?
    
    * homepage finished. Please put it live
    
    * Roles and permission changes; delete sec dashboard and trace tab
    
    * roles and permission changes
    
    * Done. Publihs please
    
    ---------
    
    Co-authored-by: rastablasta <bruno@treblle.com>

--- app/explore-treblle/workspace/_meta.tsx
+++ app/explore-treblle/workspace/_meta.tsx
@@ -1,9 +1,7 @@
 export default {
     "home": "Home",
     "apis": "APIs",
-    "trace": "Trace",
     "catalog": "Catalog",
     "developer-portal": "Developer Portal",
-    "security-dashboard": "Security Dashboard",
     "alfred-ai": "Alfred"
   }
