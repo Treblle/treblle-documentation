13242619a0b955968644e886105bada9b4645434
rastablasta <bruno@treblle.com>
2026-03-19 14:55:05 +0100

    feat: home structure

---
+++ app/explore-treblle/workspace/home/_meta.tsx
@@ -0,0 +1,7 @@
+export default {
+    "home-security": "Security",
+    "home-performance": "Performance",
+    "home-governance": "Governance",
+    "home-customers": "Customers",
+    "home-agents": "Agents"
+  }
