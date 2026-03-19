75a54498d66e3c0ae087bc8610b642fe7297d4d7
Hrvoje Cuckovic <cuckovichrvoje@gmail.com>
2026-03-19 14:59:14 +0100

    fix: commented unused pages

--- app/explore-treblle/workspace/home/_meta.tsx
+++ app/explore-treblle/workspace/home/_meta.tsx
@@ -1,7 +1,7 @@
 export default {
     "home-security": "Security",
-    "home-performance": "Performance",
-    "home-governance": "Governance",
-    "home-customers": "Customers",
-    "home-agents": "Agents"
+    // "home-performance": "Performance",
+    // "home-governance": "Governance",
+    // "home-customers": "Customers",
+    // "home-agents": "Agents"
   }
