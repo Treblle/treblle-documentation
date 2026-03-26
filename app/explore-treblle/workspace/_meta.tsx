805bc32e967d11730fe28f7b0a21ef535e4a26de
Hrvoje <cuckovichrvoje@gmail.com>
2026-03-26 13:18:25 +0100

    fix: Old pages delete
    * bruno test homepage
    
    * bruno test
    
    * bruno test 2
    
    * ispravljeno final
    
    * is it finall over?
    
    * now finally over?
    
    * homepage finished. Please put it live
    
    * Roles and permission changes; delete sec dashboard and trace tab
    
    * roles and permission changes
    
    * Done. Publihs please
    
    * typo fixing
    
    * feat: home structure
    
    * fix: commented unused pages
    
    * fix: fixed home page link and added home page
    
    * feat: readme for docs
    
    * Gotovi svi pageovi za home-cucko push
    
    * sad je gotovo zaprave, bugovi ispravljeni
    
    * fix: fixed conflict
    
    * home overview changes
    
    * Delete old pages; checked with Dubravko. Push changes
    
    * fix: fix _meta fila
    
    * fix:fix _meta
    
    * fix: fixed deleted pages
    
    ---------
    
    Co-authored-by: rastablasta <bruno@treblle.com>

--- app/explore-treblle/workspace/_meta.tsx
+++ app/explore-treblle/workspace/_meta.tsx
@@ -1,7 +1,5 @@
 export default {
     "home": "Home",
     "apis": "APIs",
-    "catalog": "Catalog",
-    "developer-portal": "Developer Portal",
-    "alfred-ai": "Alfred"
+    "catalog": "Catalog"
   }
